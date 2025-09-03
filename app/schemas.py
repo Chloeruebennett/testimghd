d0914a25114e1c3b49df645789eb5feab11ace4d
Chloeruebennett <dzdz67204@gmail.com>
2025-09-03 13:20:53 +0500

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+# Пользователь
+class UserCreate(BaseModel):
+    username: str = Field(..., max_length=50)
+    password: str = Field(..., min_length=6)
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+# Токен
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+# Заметка
+class NoteCreate(BaseModel):
+    title: str
+    content: Optional[str] = None
+
+class NoteRead(BaseModel):
+    id: int
+    title: str
+    content: Optional[str]
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
